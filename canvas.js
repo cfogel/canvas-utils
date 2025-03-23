b56ad36d6290840a3e47880a600391eb9c8e21e3
CJ Fogel <fogelcj@gmail.com>
2025-03-23 16:08:04 -0400

    Initial commit

---
+++ canvas.js
@@ -0,0 +1,55 @@
+import { CANVAS_TOKEN, CANVAS_ENDPOINT } from "./config.js";
+export const CANVAS_GET_INIT = {
+    method: "GET",
+    headers: {
+        Authorization: `Bearer ${CANVAS_TOKEN}`
+    }
+};
+
+export const arrayParams = (key, values) => `${key}[]=${values.join(`&${key}[]=`)}`;
+export const canvasParams = ({ include, fields, exclude_fields, per_page, ...rest }) => [
+    ...(include ? [arrayParams('include', include)] : []),
+    ...(fields ? [arrayParams('response_fields', fields)] : []),
+    ...(exclude_fields ? [arrayParams('exclude_response_fields', exclude_fields)] : []),
+    ...(per_page ? [`per_page=${per_page}`] : []),
+    ...(Object.entries(rest).map(([k, v]) => `${k}=${v}`))
+].join('&');
+export const headers = (token, ctype) => Object.assign({
+    Authorization: `Bearer ${token}`
+}, ctype ? {
+    'Content-Type': ctype
+} : {});
+export const getPaginated = async (resource, options, list = []) => {
+    const r = await fetch(resource, options);
+    const nextLink = r.headers.get('link').split(',').find(l => l.includes('rel="next"'));
+    return nextLink ? getPaginated(/^<([^>]*)>/.exec(nextLink)[1], options, [...list, ...(await r.json())]) : [...list, ...(await r.json())]
+}
+
+/* Assignments */
+
+export const getAssignment = (course, assignment, { include, fields, exclude_fields, ...rest }) => fetch(`${CANVAS_ENDPOINT}/api/v1/courses/${course}/assignments/${assignment}?${canvasParams({include,fields,exclude_fields,...rest})}`, CANVAS_GET_INIT).then(r => r.json());
+export const listAssignments = (course, { include, fields, exclude_fields, per_page = 100, assignment_ids, ...rest }) => getPaginated(`${CANVAS_ENDPOINT}/api/v1/courses/${course}/assignments?${canvasParams({include,fields,exclude_fields,per_page,...rest})}${assignment_ids ? `&${arrayParams('assignment_ids',assignment_ids)}` : ''}`, CANVAS_GET_INIT);
+
+/* Courses */
+
+export const listCourses = ({ include, fields, exclude_fields, per_page = 100, state, ...rest }) => getPaginated(`${CANVAS_ENDPOINT}/api/v1/courses?${canvasParams({include,fields,exclude_fields,per_page,...rest})}${state ? `&${arrayParams('state',state)}` : ''}`, CANVAS_GET_INIT);
+export const getCourse = (course, { include, fields, exclude_fields, ...rest }) => fetch(`${CANVAS_ENDPOINT}/api/v1/courses/${course}?${canvasParams({include,fields,exclude_fields,...rest})}`, CANVAS_GET_INIT).then(r => r.json());
+export const getCoursePermissions = (course, { permissions, fields, exclude_fields }) => fetch(`${CANVAS_ENDPOINT}/api/v1/courses/${course}/permissions?${permissions ? `${arrayParams('permissions',permissions)}&` : ''}${canvasParams({fields,exclude_fields})}`, CANVAS_GET_INIT).then(r => r.json());
+export const listCourseUsers = (course, { include, fields, exclude_fields, per_page = 100, enrollment_type, users, enrollment_state, ...rest } = {}) => getPaginated(`${CANVAS_ENDPOINT}/api/v1/courses/${course}/users?${canvasParams({include,fields,exclude_fields,per_page,...rest})}${enrollment_type ? `&${arrayParams('enrollment_type',enrollment_type)}` : ''}${users ? `&${arrayParams('user_ids',users)}` : ''}${enrollment_state ? `&${arrayParams('enrollment_state',enrollment_state)}` : ''}`, CANVAS_GET_INIT);
+
+/* Submissions */
+
+export const updateGrades = (course, assignment, updates) => fetch(`${CANVAS_ENDPOINT}/api/v1/courses/${course}/assignments/${assignment}/submissions/update_grades`, {
+    method: "POST",
+    headers: headers(CANVAS_TOKEN, 'application/json'),
+    body: JSON.stringify(updates)
+}).then(r => r.json());
+
+export const listSubmissions = (course, { students, assignments, include = ['user','assignment','submission_comments','submission_history'], fields, exclude_fields, per_page = 25, ...rest} = {}) => getPaginated(`${CANVAS_ENDPOINT}/api/v1/courses/${course}/students/submissions?${arrayParams('student_ids',students ?? ['all'])}&${canvasParams({include,fields,exclude_fields,per_page,...rest,grouped:true})}${assignments ? `&${arrayParams('assignment_ids',assignments)}` : ''}`, CANVAS_GET_INIT).then(r => r.flatMap(s => s.submissions));
+export const getAssignmentSubmissions = (course, assignment, { include = ['user','submission_comments','submission_history'], fields, exclude_fields, per_page = 100, ...rest } = {}) => getPaginated(`${CANVAS_ENDPOINT}/api/v1/courses/${course}/assignments/${assignment}/submissions?${canvasParams({include,fields,exclude_fields,per_page,...rest})}`, CANVAS_GET_INIT);
+export const getSubmission = (course, assignment, user, { include = ['user','assignment','submission_comments','submission_history'], fields = [], exclude_fields = []} = {}) => fetch(`${CANVAS_ENDPOINT}/api/v1/courses/${course}/assignments/${assignment}/submissions/${user}?${canvasParams({include,fields,exclude_fields})}`, CANVAS_GET_INIT).then(r => r.json());
+
+/* Progress */
+
+export const getProgress = id => fetch(`${CANVAS_ENDPOINT}/api/v1/progress/${id}`, CANVAS_GET_INIT).then(r => r.json());
+
---
+++ gradescope.js
@@ -0,0 +1,17 @@
+import { CANVAS_GET_INIT, canvasParams, getAssignmentSubmissions, getPaginated } from "./canvas.js";
+import { CANVAS_ENDPOINT, COURSE_ID, MISSING_EXEMPT } from "./config.js";
+
+export const csvToUpdates = async (csv=CSV,course=COURSE_ID) => {
+    const users = await getPaginated(`${CANVAS_ENDPOINT}/api/v1/courses/${course}/users?${canvasParams({fields:['email','id'],per_page:100})}`,CANVAS_GET_INIT)
+    const scores = (Array.isArray(csv) ? csv : csv.split('\n').map(s=>s.split(','))).slice(1)
+        .filter(([,,,,,score])=>score!='')
+        .map(([,,,email,,score])=>({email,score,id:users.find(user=>user.email.toLowerCase()==email)?.id}))
+        .filter(({id})=>id)
+    return ({grade_data:Object.fromEntries(scores.map(({score,id})=>[id,{posted_grade:score}]))})
+}
+
+export const missingUpdates = async (assignment=ASSIGNMENT,course=COURSE_ID,ex=MISSING_EXEMPT) => {
+    const submissions = await getAssignmentSubmissions(course,assignment,{include:['user'],fields:['user_id','score','user']})
+    const missing = submissions.filter(({score,user_id})=>!score&&!ex.includes(user_id))
+    return ({grade_data: Object.fromEntries(missing.map(s=>[s.user_id,({posted_grade:0})]))})
+}
