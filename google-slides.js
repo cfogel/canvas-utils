286752d23b8156357dcf1b044cee71c33517d764
CJ Fogel <fogelcj@gmail.com>
2025-03-27 17:18:14 -0400

    Add Google Slides utilities

---
+++ google-slides.js
@@ -0,0 +1,9 @@
+const parseResponse = r => r.startsWith(")]}'") ? JSON.parse(r.substring(4)) : JSON.parse(r);
+const expandChangelog = changelog => [...changelog.filter(([[t]])=>t!=4),...changelog.filter(([[t]])=>t==4).flatMap(([[,e],...rest])=>e.map(e=>[e,...rest]))].toSorted(([,a],[,b])=>a-b)
+const parseEntry = userMap => (([[type,slide,,sIdx,v],timestamp,user]) => type == 15 ? ({type: 'Insertion',slide,sIdx,value:v,timestamp:new Date(timestamp),user:userMap[user]?.name,length:v.length}) : type == 16 ? ({type: 'Deletion',slide,sIdx,eIdx:v,timestamp:new Date(timestamp),user:userMap[user]?.name,length:v-sIdx+1}) : null)
+
+const getChangelog = async (id=/\/(?<id>[^/]*)\/edit/.exec(document.location.pathname).groups.id,token=_docs_flag_initialData.info_params.token) => {
+    const { tileInfo, userMap } = parseResponse(await fetch(`https://docs.google.com/presentation/d/${id}/revisions/tiles?id=${id}&token=${token}`).then(r => r.text()));
+    const { changelog } = parseResponse(await fetch(`https://docs.google.com/presentation/d/${id}/revisions/load?id=${id}&start=1&end=${tileInfo.at(-1).end.toString()}&token=${token}`).then(r => r.text()));
+    return expandChangelog(changelog).filter(([[t]])=>[15,16].includes(t)).map(parseEntry(userMap))
+}
