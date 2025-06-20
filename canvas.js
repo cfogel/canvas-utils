b8e9eb45dc8ad8d2b2590bbace2a164637dabdfc
CJ Fogel <fogelcj@gmail.com>
2025-06-20 12:07:25 -0400

    Formatting improvements

--- canvas.js
+++ canvas.js
@@ -21,7 +21,7 @@ export const headers = (token, ctype) => Object.assign({
 export const getPaginated = async (resource, options, list = []) => {
     const r = await fetch(resource, options);
     const nextLink = r.headers.get('link').split(',').find(l => l.includes('rel="next"'));
-    return nextLink ? getPaginated(/^<([^>]*)>/.exec(nextLink)[1], options, [...list, ...(await r.json())]) : [...list, ...(await r.json())]
+    return nextLink ? getPaginated(/^<([^>]*)>/.exec(nextLink)[1], options, [...list, ...(await r.json())]) : [...list, ...(await r.json())];
 }
 export const getPaginatedObj = async (resource, options, list = {}) => {
     const r = await fetch(resource, options);
@@ -54,10 +54,10 @@ export const listCourseUsers = (course, { include, fields, exclude_fields, per_p
 
 export const getGradeChanges = async (course, { assignment, student_id, grader_id, fields, exclude_fields, per_page = 100, ...rest } = {}) => {
     const { events, linked } = await getPaginatedObj(`${CANVAS_ENDPOINT}/api/v1/audit/grade_change?course_id=${course}&${canvasParams({assignment_id:assignment,student_id,grader_id,fields,exclude_fields,per_page,...rest})}`, CANVAS_GET_INIT);
-    return events.map(({links:{course,student,grader,page_view,assignment},event_type,id,...rest})=>({student: linked.users?.find(({id})=>id==student)?.name, assignment: linked.assignments?.find(({id})=>id==assignment)?.name, ...rest, grader: linked.users?.find(({id})=>id==grader)?.name, id}))
+    return events.map(({links:{course,student,grader,page_view,assignment},event_type,id,...rest})=>({student: linked.users?.find(({id})=>id==student)?.name, assignment: linked.assignments?.find(({id})=>id==assignment)?.name, ...rest, grader: linked.users?.find(({id})=>id==grader)?.name, id}));
 }
 
-export const graderCounts = changes => Object.entries(Object.groupBy(changes,s=>s.grader)).map(([k,v])=>[k,[...new Set(v.map(s=>s.student))]])
+export const graderCounts = changes => Object.entries(Object.groupBy(changes,s=>s.grader)).map(([k,v])=>[k,[...new Set(v.map(s=>s.student))]]);
 
 /* Submissions */
 
@@ -70,10 +70,10 @@ export const updateGrades = (course, assignment, updates) => fetch(`${CANVAS_END
 export const listSubmissions = (course, { students, assignments, include = ['user','assignment','submission_comments','submission_history'], fields, exclude_fields, per_page = 25, ...rest} = {}) => getPaginated(`${CANVAS_ENDPOINT}/api/v1/courses/${course}/students/submissions?${canvasParams({include,fields,exclude_fields,per_page,'student_ids[]':students ?? ['all'],'assignment_ids[]':assignments,...rest,grouped:true})}`, CANVAS_GET_INIT).then(r => r.flatMap(s => s.submissions));
 export const getAssignmentSubmissions = (course, assignment, { include = ['user','submission_comments','submission_history'], fields, exclude_fields, per_page = 100, ...rest } = {}) => getPaginated(`${CANVAS_ENDPOINT}/api/v1/courses/${course}/assignments/${assignment}/submissions?${canvasParams({include,fields,exclude_fields,per_page,...rest})}`, CANVAS_GET_INIT);
 export const getSubmission = (course, assignment, user, { include = ['user','assignment','submission_comments','submission_history'], fields = [], exclude_fields = []} = {}) => fetch(`${CANVAS_ENDPOINT}/api/v1/courses/${course}/assignments/${assignment}/submissions/${user}?${canvasParams({include,fields,exclude_fields})}`, CANVAS_GET_INIT).then(r => r.json());
-export const getExcusedUsers = async (course, assignment, extraUsers=[]) => [...extraUsers, ...((await getAssignmentSubmissions(course,assignment,{include:null,fields:['user_id','excused']})).filter(({excused})=>excused).map(({user_id})=>user_id))]
+export const getExcusedUsers = async (course, assignment, extraUsers=[]) => [...extraUsers, ...((await getAssignmentSubmissions(course,assignment,{include:null,fields:['user_id','excused']})).filter(({excused})=>excused).map(({user_id})=>user_id))];
 
-export const expandSubmissionHistory = submissions => [...submissions,...submissions.filter(({attempt})=>attempt>1).flatMap(({submission_history,assignment,user,submission_comments})=>submission_history.slice(0,-1).map(s=>({...s,submission_history,submission_comments,assignment,user,old_attempt:true})))].toSorted((a,b)=>a.id-b.id)
-export const formatSubmission = ({id, user:{name} = {}, assignment:{name:assignment} = {}, ...s}) => ({id,name,assignment,...s})
+export const expandSubmissionHistory = submissions => [...submissions,...submissions.filter(({attempt})=>attempt>1).flatMap(({submission_history,assignment,user,submission_comments})=>submission_history.slice(0,-1).map(s=>({...s,submission_history,submission_comments,assignment,user,old_attempt:true})))].toSorted((a,b)=>a.id-b.id);
+export const formatSubmission = ({id, user:{name} = {}, assignment:{name:assignment} = {}, ...s}) => ({id,name,assignment,...s});
 
 /* Progress */
 
@@ -82,4 +82,4 @@ export const getProgress = id => fetch(`${CANVAS_ENDPOINT}/api/v1/progress/${id}
 /* Analytics */
 
 export const getUserParticipation = (course, user) => fetch(`${CANVAS_ENDPOINT}/api/v1/courses/${course}/analytics/users/${user}/activity`,CANVAS_GET_INIT).then(r=>r.json());
-export const userAccessReports = (course, batchSize=1) => listCourseUsers(course).then(users => batchMap(async ({id,name}) => [name, await fetch(`${CANVAS_ENDPOINT}/courses/${course}/users/${id}/usage`).then(r=>r.text()).then(r=>[...r.replaceAll(/\n/g,'').matchAll(/<tr.*?readable_name">(?<content>[^<]*).*?view_score">(?<timesViewed>[^<]*).*?participate_score">(?<timesParticipated>[^<]*).*?data-timestamp="(?<lastViewed>[^"]*).*?<\/tr>/g).map(l=>l.groups)])],users,batchSize))
+export const userAccessReports = (course, batchSize=1) => listCourseUsers(course).then(users => batchMap(async ({id,name}) => [name, await fetch(`${CANVAS_ENDPOINT}/courses/${course}/users/${id}/usage`).then(r=>r.text()).then(r=>[...r.replaceAll(/\n/g,'').matchAll(/<tr.*?readable_name">(?<content>[^<]*).*?view_score">(?<timesViewed>[^<]*).*?participate_score">(?<timesParticipated>[^<]*).*?data-timestamp="(?<lastViewed>[^"]*).*?<\/tr>/g).map(l=>l.groups)])],users,batchSize));
--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 export const removeprops = props => (o => props.reduce((acc,v)=>(({[v]:acc, ...v} = acc), v),o));
 export const appendObj = (o,n) => ({...o,...Object.fromEntries(Object.entries(n).map(([k,v])=>Array.isArray(o[k]) ? [k,[...o[k],...v]] : ((o[k]&&v&&(typeof v=='object')&&(typeof o[k]=='object')) ? [k,appendObj(o[k],v)] : [k,v])))});
-export const batchMap = (func,data,batchSize=100,res=[]) => Promise.all(data.slice(0,batchSize).map(func)).then(r=> data.length > batchSize ? batchMap(func,data.slice(batchSize),batchSize,[...res,...r]) : [...res,...r])
+export const batchMap = (func,data,batchSize=100,res=[]) => Promise.all(data.slice(0,batchSize).map(func)).then(r=> data.length > batchSize ? batchMap(func,data.slice(batchSize),batchSize,[...res,...r]) : [...res,...r]);
